bf3fd8f1c81af329deda1e71c19502e342fcf9e6
dbmiddle <dbmiddle39@gmail.com>
2020-06-19 13:30:47 -0400

    set up a component to display shoe details from backend api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoes: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/shoe/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ shoes: data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Shoe Details</h1>
+        <ul>
+          {this.state.shoes.map((shoe) => {
+            return (
+              <>
+                <li>Shoe Size: {shoe.size}</li>
+                <li>Brand: {shoe.brand_name}</li>
+                <li>Manufacturer: {shoe.manufacturer}</li>
+                <li>Color: {shoe.color}</li>
+                <li>Material: {shoe.material}</li>
+                <li>Type: {shoe.shoe_type}</li>
+                <li>Fasten Type: {shoe.fasten_type}</li>
+                <br/>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
